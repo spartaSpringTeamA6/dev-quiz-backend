c4c69264c3ef6985e6c20bc401ce857ddf9a9a86
jiisuniui <dudndi9752@gmail.com>
2024-01-08 16:17:33 +0900

    🎉 create: create project (#1)

---
+++ src/main/java/com/sparta/devquiz/DevQuizApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.devquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevQuizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevQuizApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/devquiz/DevQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.devquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevQuizApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
