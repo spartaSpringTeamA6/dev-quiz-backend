8f3ecc1a952a380437ca944867155bc43d2c1d67
Youkamii <gkfkd747@naver.com>
2024-01-09 20:48:06 +0900

    ✨ feat(Board): 보드 단일 조회 BoardSingleGetResponseDto 작성 (#8)

--- src/main/java/com/sparta/devquiz/domain/board/dto/BoardSingleGetResponseDto.java
+++ src/main/java/com/sparta/devquiz/domain/board/dto/BoardSingleGetResponseDto.java
@@ -1,26 +1,18 @@
 package com.sparta.devquiz.domain.board.dto;
 
 import com.sparta.devquiz.domain.board.entity.Board;
-import com.sparta.devquiz.domain.board.response.BoardResponseCode;
-import com.sparta.devquiz.global.response.CommonResponseDto;
 import lombok.Getter;
 
 @Getter
-public class BoardSingleGetResponseDto extends CommonResponseDto<Board> {
-    private Long boardId;
-    private String title;
-    private String content;
-    private Long userId;
-    private Long quizId;
+public class BoardSingleGetResponseDto {
 
-    public BoardSingleGetResponseDto(Board board) {
-        super(BoardResponseCode.OK_GET_BOARD_INFO.getHttpStatus(),
-                BoardResponseCode.OK_GET_BOARD_INFO.getMessage(),
-                board);
-        this.boardId = board.getId();
-        this.title = board.getTitle();
-        this.content = board.getContent();
-        this.userId = board.getUser().getId();
-        this.quizId = board.getQuiz().getId();
+    private String statusCode;
+    private String message;
+    private Board data;
+
+    public BoardSingleGetResponseDto(String statusCode, String message, Board data) {
+        this.statusCode = statusCode;
+        this.message = message;
+        this.data = data;
     }
 }
