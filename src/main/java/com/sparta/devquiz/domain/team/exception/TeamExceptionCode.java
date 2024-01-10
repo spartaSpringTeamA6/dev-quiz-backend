57aade7f1d4443a29ec26b55543320fd1d7a4dc1
jiisuniui <dudndi9752@gmail.com>
2024-01-10 11:09:03 +0900

    :sparkles: feat(Team): TeamExceptionCode 추가작성 (#6)

--- src/main/java/com/sparta/devquiz/domain/team/exception/TeamExceptionCode.java
+++ src/main/java/com/sparta/devquiz/domain/team/exception/TeamExceptionCode.java
@@ -11,9 +11,11 @@ public enum TeamExceptionCode {
     // BAD REQUEST 400
 
     // NOT FOUND 404
-    NOT_FOUND_TEAM(HttpStatus.NOT_FOUND, "TEAM-001", "해당 팀을 찾을 수 없습니다.");
+    NOT_FOUND_TEAM(HttpStatus.NOT_FOUND, "TEAM-001", "해당 팀을 찾을 수 없습니다."),
 
     // FORBIDDEN 403
+    FORBIDDEN_TEAM_USER(HttpStatus.FORBIDDEN, "TEAM-002", "해당 유저는 팀 유저 권한이 없습니다."),
+    FORBIDDEN_TEAM_ADMIN(HttpStatus.FORBIDDEN, "TEAM-003", "해당 유저는 팀 관리자 권한이 없습니다."),;
 
     // CONFLICT 409
 
