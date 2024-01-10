e0b23d28523c243c42dddf55e96d2dbb53b628aa
jiisuniui <dudndi9752@gmail.com>
2024-01-10 10:53:56 +0900

    :sparkles: feat(Team): UserTeamRepo feat (#6)

--- src/main/java/com/sparta/devquiz/domain/team/repository/UserTeamRepository.java
+++ src/main/java/com/sparta/devquiz/domain/team/repository/UserTeamRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserTeamRepository extends JpaRepository<UserTeam, Long> {
 
+    Boolean existsByUserIdAndTeamIdAndIsAcceptedTrue(Long userId, Long teamId);
 }
