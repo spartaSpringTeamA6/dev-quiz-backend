3a032892ece599f4893197030c453bdf042b956c
jiisuniui <dudndi9752@gmail.com>
2024-01-10 18:09:00 +0900

    :recycle: refactor(Team): dto refactor (#6)

--- src/main/java/com/sparta/devquiz/domain/team/dto/request/TeamCreateRequest.java
+++ src/main/java/com/sparta/devquiz/domain/team/dto/request/TeamCreateRequest.java
@@ -3,10 +3,8 @@ package com.sparta.devquiz.domain.team.dto.request;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
 @Schema(description = "팀 생성 요청 dto")
 public class TeamCreateRequest {
 
--- src/main/java/com/sparta/devquiz/domain/team/dto/request/TeamGetRequest.java
+++ src/main/java/com/sparta/devquiz/domain/team/dto/request/TeamGetRequest.java
@@ -1,12 +1,9 @@
 package com.sparta.devquiz.domain.team.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
 @Schema(description = "팀 조회 요청 dto")
 public class TeamGetRequest {
 
