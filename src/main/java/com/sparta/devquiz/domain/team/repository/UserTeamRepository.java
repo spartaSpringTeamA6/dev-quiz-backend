0e6db380890b13974743d1b48de8ed1b75606129
jiisuniui <dudndi9752@gmail.com>
2024-01-10 10:51:47 +0900

    :sparkles: feat(Team): UserTeamRepo create (#6)

---
+++ src/main/java/com/sparta/devquiz/domain/team/repository/UserTeamRepository.java
@@ -0,0 +1,8 @@
+package com.sparta.devquiz.domain.team.repository;
+
+import com.sparta.devquiz.domain.team.entity.UserTeam;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserTeamRepository extends JpaRepository<UserTeam, Long> {
+
+}
