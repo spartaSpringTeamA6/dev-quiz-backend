38b236b40e484067df35b94c2d8e9a458c2f0572
jiisuniui <dudndi9752@gmail.com>
2024-01-10 11:44:11 +0900

    :sparkles: feat(Team): TeamUserService saveTeamUser, saveTeamAdmin (#6)

--- src/main/java/com/sparta/devquiz/domain/team/service/TeamUserService.java
+++ src/main/java/com/sparta/devquiz/domain/team/service/TeamUserService.java
@@ -1,6 +1,7 @@
 package com.sparta.devquiz.domain.team.service;
 
 import com.sparta.devquiz.domain.team.entity.Team;
+import com.sparta.devquiz.domain.team.entity.TeamUser;
 import com.sparta.devquiz.domain.team.enums.TeamUserRole;
 import com.sparta.devquiz.domain.team.repository.TeamUserRepository;
 import com.sparta.devquiz.domain.user.entity.User;
@@ -16,7 +17,15 @@ public class TeamUserService {
     private final TeamUserRepository teamUserRepository;
 
     public void saveTeamUser(Team team, User user){
+        TeamUser teamUser = TeamUser.builder().team(team).user(user)
+                .userRole(TeamUserRole.USER).build();
+        teamUserRepository.save(teamUser);
+    }
 
+    public void saveTeamAdmin(Team team, User user){
+        TeamUser teamUser = TeamUser.builder().team(team).user(user)
+                .userRole(TeamUserRole.ADMIN).build();
+        teamUserRepository.save(teamUser);
     }
 
     public Boolean isExistedUser(Team team, User user){
