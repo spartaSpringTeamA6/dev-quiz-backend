82a7d91456630d32aed143dac98e89139538a54e
Youkamii <gkfkd747@naver.com>
2024-01-14 22:58:32 +0900

    :recycle: refactor(Quiz): CommentLikeRepository 수정 (#11)

--- src/main/java/com/sparta/devquiz/domain/comment/repository/CommentLikeRepository.java
+++ src/main/java/com/sparta/devquiz/domain/comment/repository/CommentLikeRepository.java
@@ -4,7 +4,7 @@ import com.sparta.devquiz.domain.comment.entity.CommentLike;
 import com.sparta.devquiz.domain.comment.entity.CommentLikeId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CommentLikeRepository extends JpaRepository<CommentLike, Long> {
+public interface CommentLikeRepository extends JpaRepository<CommentLike, CommentLikeId> {
     boolean existsById(CommentLikeId commentLikeId);
 
     void deleteById(CommentLikeId commentLikeId);
