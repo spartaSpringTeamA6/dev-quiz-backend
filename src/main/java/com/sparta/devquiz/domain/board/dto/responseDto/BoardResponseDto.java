2fc4e8d4318b07dce27b48ce7d2869ad77469895
Youkamii <gkfkd747@naver.com>
2024-01-14 23:07:22 +0900

    :sparkles: feat(Board): BoardResponseDto (#8)

---
+++ src/main/java/com/sparta/devquiz/domain/board/dto/responseDto/BoardResponseDto.java
@@ -0,0 +1,10 @@
+package com.sparta.devquiz.domain.board.dto.responseDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class BoardResponseDto {
+
+}
