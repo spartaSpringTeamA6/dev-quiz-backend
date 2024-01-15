34a6888baec029c43f17bdbd13ad947c0c877e7b
Youkamii <gkfkd747@naver.com>
2024-01-15 16:06:30 +0900

    :sparkles: feat(Comment): BoardCreateResponseDto (#11)

--- src/main/java/com/sparta/devquiz/domain/board/dto/responseDto/BoardCreateResponseDto.java
+++ src/main/java/com/sparta/devquiz/domain/board/dto/responseDto/BoardCreateResponseDto.java
@@ -2,10 +2,8 @@ package com.sparta.devquiz.domain.board.dto.responseDto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 @AllArgsConstructor
 public class BoardCreateResponseDto {
     private Long boardId;
