b2bec2a1d381b42426936112e3278c56d481bbe6
Youkamii <gkfkd747@naver.com>
2024-01-16 18:01:47 +0900

    :sparkles: feat(Comment): CommentResponseCode (#11)

--- src/main/java/com/sparta/devquiz/domain/comment/response/CommentResponseCode.java
+++ src/main/java/com/sparta/devquiz/domain/comment/response/CommentResponseCode.java
@@ -10,18 +10,18 @@ import org.springframework.http.HttpStatus;
 public enum CommentResponseCode implements ResponseCode {
 
     // OK 200
-    OK_GET_ALL_COMMENT(HttpStatus.OK.value(), "GET ALL COMMENTS"),
+    OK_GET_ALL_COMMENT(HttpStatus.OK, "GET ALL COMMENTS"),
 
     // CREATED 201
-    CREATED_COMMENT(HttpStatus.CREATED.value(), "SUCCESS TO CREATE COMMENT"),
-    CREATED_LIKE(HttpStatus.CREATED.value(), "SUCCESS TO LIKE COMMENT"),
+    CREATED_COMMENT(HttpStatus.CREATED, "SUCCESS TO CREATE COMMENT"),
+    CREATED_LIKE(HttpStatus.CREATED, "SUCCESS TO LIKE COMMENT"),
 
     // OK 204:  기존 RESPONSE CODE 200에서  DATA가 "" 인 것들을 204로 사용
-    OK_UPDATE_COMMENT(HttpStatus.NO_CONTENT.value(), "SUCCESS TO UPDATE COMMENT"),
-    OK_DELETE_COMMENT(HttpStatus.NO_CONTENT.value(), "SUCCESS TO DELETE COMMENT"),
-    OK_DELETE_LIKE(HttpStatus.NO_CONTENT.value(), "SUCCESS TO DELETE LIKE");
+    OK_UPDATE_COMMENT(HttpStatus.NO_CONTENT, "SUCCESS TO UPDATE COMMENT"),
+    OK_DELETE_COMMENT(HttpStatus.NO_CONTENT, "SUCCESS TO DELETE COMMENT"),
+    OK_DELETE_LIKE(HttpStatus.NO_CONTENT, "SUCCESS TO DELETE LIKE");
 
-    private final Integer httpStatus;
+    private final HttpStatus httpStatus;
     private final String message;
 
 }
