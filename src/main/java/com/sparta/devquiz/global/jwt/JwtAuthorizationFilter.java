a83393018ace576cf0eb6eac275e203b8588d924
jiisuniui <dudndi9752@gmail.com>
2024-01-16 16:04:04 +0900

    :recycle: refactor(Global): 코드 컨벤션 리팩토링 (#1)

--- src/main/java/com/sparta/devquiz/global/jwt/JwtAuthorizationFilter.java
+++ src/main/java/com/sparta/devquiz/global/jwt/JwtAuthorizationFilter.java
@@ -31,6 +31,7 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
         }
         filterChain.doFilter(request, response);
     }
+
     public void setAuthentication(String oauthId) {
         SecurityContext context = SecurityContextHolder.createEmptyContext();
         Authentication authentication = createAuthentication(oauthId);
@@ -42,4 +43,5 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
         UserDetails userDetails = userDetailsService.loadUserByUsername(oauthId);
         return new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
     }
+
 }
