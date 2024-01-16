8f9e6705c4d5921ba050f7513daa69f809b3849f
rawfk <gogitv52@gmail.com>
2024-01-17 07:01:50 +0900

    :sparkles: feat(User): Auth API 로그아웃, 재발급 기능 구현 (#4)

---
+++ src/main/java/com/sparta/devquiz/domain/user/service/AuthService.java
@@ -0,0 +1,45 @@
+package com.sparta.devquiz.domain.user.service;
+
+import static com.sparta.devquiz.global.jwt.service.JwtService.ACCESS_TOKEN_HEADER;
+
+import com.sparta.devquiz.global.jwt.service.JwtService;
+import com.sparta.devquiz.global.redis.RedisService;
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+  private final JwtService jwtService;
+  private final RedisService redisService;
+
+  public void logout(HttpServletRequest request) {
+    String accessToken = jwtService.resolveTokenFromHeader(request);
+
+    if (StringUtils.hasText(accessToken)) {
+      String subject = jwtService.getClaimsFromToken(accessToken).getSubject();
+      redisService.deleteValues(subject);
+    }
+  }
+
+  public void reissue(HttpServletRequest request, HttpServletResponse response) {
+    String refreshToken = jwtService.resolveTokenFromCookie(request);
+
+
+    if (StringUtils.hasText(refreshToken)) {
+      Claims claims = jwtService.getClaimsFromToken(refreshToken);
+      String oauthId = claims.getSubject();
+      String userRole = jwtService.getUserRole(claims);
+
+      if (redisService.hasValues(oauthId) && refreshToken.equals(redisService.getValues(oauthId))) {
+        String newAccessToken = jwtService.createAccessToken(oauthId, userRole);
+        response.setHeader(ACCESS_TOKEN_HEADER, newAccessToken);
+      }
+    }
+  }
+}
