7d446336153a8c416b53ad58f7d6c6cc9bee88d7
Youkamii <gkfkd747@naver.com>
2024-01-16 17:44:24 +0900

    :sparkles: feat(Board): Response Code 수정 (#8)

--- src/main/java/com/sparta/devquiz/domain/board/response/BoardResponseCode.java
+++ src/main/java/com/sparta/devquiz/domain/board/response/BoardResponseCode.java
@@ -10,17 +10,25 @@ import org.springframework.http.HttpStatus;
 public enum BoardResponseCode implements ResponseCode {
 
     // OK 200
-    OK_GET_BOARDLIST_INFO(HttpStatus.OK.value(), "SUCCESS TO GET BOARDLIST"),
-    OK_GET_BOARD_INFO(HttpStatus.OK.value(), "SUCCESS TO GET BOARD"),
+    OK_GET_BOARDLIST_INFO(HttpStatus.OK, "SUCCESS TO GET BOARDLIST"),
+    OK_GET_BOARD_INFO(HttpStatus.OK, "SUCCESS TO GET BOARD"),
 
     // CREATED 201
-    CREATED_BOARD(HttpStatus.CREATED.value(), "SUCCESS TO CREATE BOARD"),
+    CREATED_BOARD(HttpStatus.CREATED, "SUCCESS TO CREATE BOARD"),
 
-    // NO CONTENT 204:  기존 RESPONSE CODE 200에서 DATA가 "" 인 것들을 204로 사용
-    OK_UPDATE_BOARD(HttpStatus.NO_CONTENT.value(), "SUCCESS TO UPDATE BOARD"),
-    OK_DELETE_BOARD(HttpStatus.NO_CONTENT.value(), "SUCCESS TO DELETE BOARD");
+    // OK 204:  기존 RESPONSE CODE 200에서 DATA가 "" 인 것들을 204로 사용
+    OK_UPDATE_BOARD(HttpStatus.NO_CONTENT, "SUCCESS TO UPDATE BOARD"),
+    OK_DELETE_BOARD(HttpStatus.NO_CONTENT, "SUCCESS TO DELETE BOARD");
 
-    private final Integer httpStatus;
+    private final HttpStatus httpStatus;
     private final String message;
+
+    public HttpStatus getHttpStatus(BoardResponseCode responseCode){
+        return responseCode.getHttpStatus();
+    }
+
+    public String getMessage(BoardResponseCode responseCode){
+        return responseCode.getMessage();
+    }
 
 }
