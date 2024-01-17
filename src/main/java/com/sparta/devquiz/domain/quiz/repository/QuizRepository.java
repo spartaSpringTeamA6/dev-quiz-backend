399be71403ecb51512356dd38a02babcfc3c5ba5
ToToRo_1106 <11wjdfk@naver.com>
2024-01-18 03:13:58 +0900

    🚑HOTFIX (Quiz): 퀴즈정답제출 인식 못하던거 수정 (#5)

--- src/main/java/com/sparta/devquiz/domain/quiz/repository/QuizRepository.java
+++ src/main/java/com/sparta/devquiz/domain/quiz/repository/QuizRepository.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
 
-@Repository
 public interface QuizRepository extends JpaRepository<Quiz, Long> {
 
     @Override
--- src/main/java/com/sparta/devquiz/domain/quiz/repository/QuizUserRepository.java
+++ src/main/java/com/sparta/devquiz/domain/quiz/repository/QuizUserRepository.java
@@ -1,9 +1,9 @@
 package com.sparta.devquiz.domain.quiz.repository;
 
-import com.sparta.devquiz.domain.team.entity.Team;
+import com.sparta.devquiz.domain.quiz.entity.UserQuiz;
+import com.sparta.devquiz.domain.quiz.entity.UserQuizId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface QuizUserRepository extends JpaRepository<Team, Long> {
-
+public interface QuizUserRepository extends JpaRepository<UserQuiz, UserQuizId> {
 
 }
