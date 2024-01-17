6f4f1de8aae37cf42b2a47da4c998db34175ca2c
ToToRo_1106 <11wjdfk@naver.com>
2024-01-17 10:50:18 +0900

    ✨feat (Quiz): 퀴즈 10개 내기 Dto 생성 (#5)

---
+++ src/main/java/com/sparta/devquiz/domain/quiz/dto/response/QuizRandomResponse.java
@@ -0,0 +1,44 @@
+package com.sparta.devquiz.domain.quiz.dto.response;
+
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@Schema(description = "랜덤 퀴즈에 대한 응답 dto")
+public class QuizRandomResponse {
+
+    @ArraySchema(arraySchema = @Schema(description = "선택된 카테고리의 랜덤 퀴즈 목록"))
+    private final List<QuizDto> quizzes;
+
+    public QuizRandomResponse(List<QuizDto> quizzes) {
+        this.quizzes = quizzes;
+    }
+
+
+    @Schema(description = "퀴즈 DTO")
+    public static class QuizDto {
+
+        @Schema(description = "생성된 퀴즈 ID", defaultValue = "1")
+        private Long id;
+
+        @Schema(description = "생성된 문제", defaultValue = "Java에서 static 키워드의 주요 기능은 무엇입니까?")
+        private String question;
+
+        @Schema(description = "생성된 보기", defaultValue = "1. 객체의 동적 생성\n2. 메소드 오버로딩\n3. 클래스 변수 및 메소드 생성\n4. 예외 처리\n5. 인터페이스 정의")
+        private String example;
+
+        public QuizDto(Long id, String question, String example) {
+            this.id = id;
+            this.question = question;
+            this.example = example;
+        }
+
+        public static QuizDto of(Long id, String question, String example) {
+            return new QuizDto(id, question, example);
+        }
+
+    }
+}
