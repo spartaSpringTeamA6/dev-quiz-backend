e152ae1aa65ba4f3a71247c8ff81b5a220474081
rawfk <gogitv52@gmail.com>
2024-01-22 09:39:11 +0900

    ♻️ refactor(User): Cookie 방식으로 변경 (#4)

--- src/main/java/com/sparta/devquiz/domain/user/service/command/AuthService.java
+++ src/main/java/com/sparta/devquiz/domain/user/service/command/AuthService.java
@@ -1,10 +1,10 @@
 package com.sparta.devquiz.domain.user.service.command;
 
-import static com.sparta.devquiz.global.jwt.service.JwtService.ACCESS_TOKEN_HEADER;
+import static com.sparta.devquiz.global.jwt.service.JwtService.ACCESS_TOKEN_COOKIE;
+import static com.sparta.devquiz.global.jwt.service.JwtService.ACCESS_TOKEN_TIME;
 import static com.sparta.devquiz.global.jwt.service.JwtService.REFRESH_TOKEN_COOKIE;
 
 import com.sparta.devquiz.global.jwt.service.JwtService;
-import com.sparta.devquiz.global.oauth.CookieUtil;
 import com.sparta.devquiz.global.redis.RedisService;
 import io.jsonwebtoken.Claims;
 import jakarta.servlet.http.HttpServletRequest;
@@ -21,27 +21,28 @@ public class AuthService {
   private final RedisService redisService;
 
   public void logout(HttpServletRequest request) {
-    String accessToken = jwtService.resolveTokenFromHeader(request);
+    String accessToken = jwtService.getTokenFromRequest(request, ACCESS_TOKEN_COOKIE);
 
     if (StringUtils.hasText(accessToken)) {
-      String subject = jwtService.getClaimsFromToken(accessToken).getSubject();
+      String subject = jwtService.getClaimsFromToken(jwtService.subStringToken(accessToken)).getSubject();
       redisService.deleteValues(subject);
     }
   }
 
   public void reissue(HttpServletRequest request, HttpServletResponse response) {
-    String refreshToken = jwtService.resolveTokenFromCookie(request);
+    String bearerToken = jwtService.getTokenFromRequest(request, REFRESH_TOKEN_COOKIE);
 
 
-    if (StringUtils.hasText(refreshToken)) {
+    if (StringUtils.hasText(bearerToken)) {
+
+      String refreshToken = jwtService.subStringToken(bearerToken);
       Claims claims = jwtService.getClaimsFromToken(refreshToken);
       String oauthId = claims.getSubject();
       String userRole = jwtService.getUserRole(claims);
 
       if (redisService.hasValues(oauthId) && refreshToken.equals(redisService.getValues(oauthId))) {
         String newAccessToken = jwtService.createAccessToken(oauthId, userRole);
-        response.setHeader(ACCESS_TOKEN_HEADER, newAccessToken);
-        CookieUtil.addCookie(response, REFRESH_TOKEN_COOKIE, refreshToken, CookieUtil.OAUTH2_COOKIE_TIME);
+        jwtService.addToCookie(response, ACCESS_TOKEN_COOKIE, newAccessToken, ACCESS_TOKEN_TIME);
       }
     }
   }
---
+++ src/main/java/com/sparta/devquiz/global/oauth/handler/OAuth2LoginFailureHandler.java
@@ -0,0 +1,39 @@
+package com.sparta.devquiz.global.oauth.handler;
+
+import static com.sparta.devquiz.global.oauth.CookieUtil.REDIRECT_URI_COOKIE_NAME;
+import static com.sparta.devquiz.global.oauth.CookieUtil.getCookie;
+
+import com.sparta.devquiz.global.oauth.repository.CookieOAuth2RequestRepository;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@Component
+@RequiredArgsConstructor
+public class OAuth2LoginFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+
+    private final CookieOAuth2RequestRepository cookieOAuth2RequestRepository;
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+      AuthenticationException exception) throws IOException, ServletException {
+      String targetUrl = getCookie(request, REDIRECT_URI_COOKIE_NAME)
+                                    .map(Cookie::getValue)
+                                    .orElse(("/"));
+
+      targetUrl = UriComponentsBuilder.fromUriString(targetUrl)
+                                    .queryParam("error", exception.getLocalizedMessage())
+                                    .build()
+                                    .toUriString();
+
+      cookieOAuth2RequestRepository.removeAuthorizationRequestCookies(request, response);
+      getRedirectStrategy().sendRedirect(request, response, targetUrl);
+    }
+  }
