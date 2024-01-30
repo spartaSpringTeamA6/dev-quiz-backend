2aab78cabdeb495003bfc3bf88ad8d2afeac7675
jiisuniui <dudndi9752@gmail.com>
2024-01-30 12:09:49 +0900

    :sparkles: feat: getmygrass 작성 (#4)

--- src/main/java/com/sparta/devquiz/domain/quiz/dto/response/QuizSolvedGrassResponse.java
+++ src/main/java/com/sparta/devquiz/domain/quiz/dto/response/QuizSolvedGrassResponse.java
@@ -1,17 +1,9 @@
 package com.sparta.devquiz.domain.quiz.dto.response;
 
-import com.querydsl.core.annotations.QueryProjection;
-import com.querydsl.core.types.dsl.DateTemplate;
-import com.querydsl.core.types.dsl.NumberExpression;
-import com.sparta.devquiz.domain.user.dto.response.UserInfoResponse;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.sql.Date;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
-import org.springframework.cglib.core.Local;
 
 @Builder
 @Getter
